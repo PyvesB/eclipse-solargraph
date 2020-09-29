a02939cc41eb5b204e00a632ad560ab416ae9898
PyvesB <PyvesDev@gmail.com>
2020-09-29 21:43:58 +0100

    Remove leftover whitespace

--- eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/utils/LogHelper.java
+++ eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/utils/LogHelper.java
@@ -20,10 +20,10 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
 public class LogHelper {
-	
+
 	private static final Bundle BUNDLE = FrameworkUtil.getBundle(LogHelper.class);
 	private static final ILog LOGGER = Platform.getLog(BUNDLE);
-	
+
 	public static void info(String message) {
 		LOGGER.log(new Status(IStatus.INFO, BUNDLE.getSymbolicName(), message));
 	}
@@ -31,7 +31,7 @@ public class LogHelper {
 	public static void error(String message, Throwable exception) {
 		LOGGER.log(new Status(IStatus.ERROR, BUNDLE.getSymbolicName(), message, exception));
 	}
-	
+
 	private LogHelper() {
 		// Utility class.
 	}
