ebc5b94652e0616395d9743bb72f5c3aa25366e6
Pierre-Yves B. <10694593+PyvesB@users.noreply.github.com>
2022-02-20 16:28:56 +0100

    Fix Eclipse 2019-03, 2019-06, 2019-09 and 2019-12 compatibility

--- eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/launch/RubyLaunchTabGroup.java
+++ eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/launch/RubyLaunchTabGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Red Hat Inc. and others.
+ * Copyright (c) 2019-2022 Red Hat Inc. and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -9,18 +9,20 @@
  *
  * Contributors:
  *  Alexander Kurtakov  (Red Hat Inc.) - Initial implementation
+ *  Pierre-Yves B.  (pyvesdev@gmail.com) - Initial implementation
  *******************************************************************************/
 package io.github.pyvesb.eclipse_solargraph.launch;
 
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
 
 public class RubyLaunchTabGroup extends AbstractLaunchConfigurationTabGroup {
 
 	@Override
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
-		setTabs(new RubyLaunchTab(), new CommonTab());
+		setTabs(new ILaunchConfigurationTab[] { new RubyLaunchTab(), new CommonTab() });
 	}
 
 }
