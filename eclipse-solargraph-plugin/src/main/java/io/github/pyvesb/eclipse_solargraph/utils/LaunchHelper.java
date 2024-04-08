bc04d130ec3caf7b6a566341f3e0a24da9ce7712
Pierre-Yves B. <10694593+PyvesB@users.noreply.github.com>
2024-04-08 14:48:47 +0200

    Clean up LaunchHelper class

--- eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/utils/LaunchHelper.java
+++ eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/utils/LaunchHelper.java
@@ -4,13 +4,12 @@ import java.io.File;
 import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 
 public class LaunchHelper {
-
 
 	public static Job createJob(ILaunch launch, String command, String workingDirectory) {
 		return createJob(launch, command, new File(workingDirectory));
@@ -27,32 +26,28 @@ public class LaunchHelper {
 				}
 				CompletableFuture<Process> future = process.onExit();
 				// initialize a process completion future to log any non-zero process exit
-				future.defaultExecutor().execute(new Runnable() {
-					@Override
-					public void run() {
-						try {
-							if (process.isAlive()) {
-								process.waitFor();
-							}
-						} catch (InterruptedException e) {
-							LogHelper.cancelled("Process monitor interrupted: " + command);
-							return;
+				future.defaultExecutor().execute(() -> {
+					try {
+						if (process.isAlive()) {
+							process.waitFor();
 						}
-						int exc = process.exitValue();
-						if (exc != 0) {
-							String msg = String.format("Process exited non-zero (%d): %s ", exc, command);
-							LogHelper.error(msg);
-						}
+					} catch (InterruptedException e) {
+						LogHelper.cancelled("Process monitor interrupted: " + command);
+						return;
 					}
-
+					int exc = process.exitValue();
+					if (exc != 0) {
+						String msg = String.format("Process exited non-zero (%d): %s ", exc, command);
+						LogHelper.error(msg);
+					}
 				});
 				DebugPlugin.newProcess(launch, process, command);
 			} catch (CoreException e) {
 				// CoreException from exec
-				LogHelper.log(Status.ERROR, "Exception when launching process: " + command, e);
+				LogHelper.log(IStatus.ERROR, "Exception when launching process: " + command, e);
 				return;
 			}
 		});
 	}
 
-};;
+}
