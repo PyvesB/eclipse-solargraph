9f7c67de06c3a9297867c581e3745e5f2ab923c1
Pierre-Yves B. <10694593+PyvesB@users.noreply.github.com>
2024-07-15 16:34:03 +0200

    Pick up user setup more robustly on macOS, fixes #34

--- eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/utils/CommandHelper.java
+++ eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/utils/CommandHelper.java
@@ -19,10 +19,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.eclipse.core.runtime.Platform;
 
 public class CommandHelper {
+
+	private static final String MACOS_DSCL_SHELL_PREFIX = "UserShell: ";
+
+	private static final AtomicReference<String> DEFAULT_SHELL_MACOS = new AtomicReference<>();
 
 	public static String findDirectory(String executable) {
 		String executablePath = findPath(executable);
@@ -62,11 +67,42 @@ public class CommandHelper {
 	}
 
 	public static String[] getPlatformCommand(String command) {
-		return isWindows() ? new String[] { "cmd.exe", "/c", command } : new String[] { "bash", "-c", "-l", command };
+		if (isWindows()) {
+			return new String[] { "cmd.exe", "/c", command };
+		} else if (isMacOS()) {
+			String defaultShellMacOS = DEFAULT_SHELL_MACOS.get();
+			if (defaultShellMacOS == null) {
+				defaultShellMacOS = findDefaultShellMacOS();
+				DEFAULT_SHELL_MACOS.set(defaultShellMacOS);
+			}
+			return new String[] { defaultShellMacOS, "-c", "-li", command };
+		}
+		return new String[] { "bash", "-c", "-l", command };
 	}
 
 	public static boolean isWindows() {
 		return Platform.OS_WIN32.equals(Platform.getOS());
+	}
+
+	public static boolean isMacOS() {
+		return Platform.OS_MACOSX.equals(Platform.getOS());
+	}
+
+	 // Inspired from https://github.com/eclipse-wildwebdeveloper/wildwebdeveloper/blob/0447608f659c1683da4816fff185d34ef54dcf8a/org.eclipse.wildwebdeveloper.embedder.node/src/org/eclipse/wildwebdeveloper/embedder/node/NodeJSManager.java#L297
+	private static String findDefaultShellMacOS() {
+		String[] dsclCommand = new String[] { "/bin/bash", "-c", "-l", "dscl . -read ~/ UserShell" };
+		try {
+			Process process = new ProcessBuilder(dsclCommand).redirectErrorStream(true).start();
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+				String shell = reader.readLine();
+				if (shell != null && shell.startsWith(MACOS_DSCL_SHELL_PREFIX)) {
+					return shell.substring(MACOS_DSCL_SHELL_PREFIX.length());
+				}
+			}
+		} catch (IOException e) {
+			LogHelper.error("Failed to find location of default macOS shell, using '/bin/zsh' instead.", e);
+		}
+		return "/bin/zsh"; // Default shell since macOS 10.15.
 	}
 
 	private CommandHelper() {
