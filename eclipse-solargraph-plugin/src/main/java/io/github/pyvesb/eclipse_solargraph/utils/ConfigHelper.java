4cfd3a5aa75cc780ac6229995ce5f4337159e2b4
Pierre-Yves B. <10694593+PyvesB@users.noreply.github.com>
2024-07-17 11:57:49 +0200

    Add missing license headers

--- eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/utils/ConfigHelper.java
+++ eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/utils/ConfigHelper.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Sean Champ and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Sean Champ - Initial implementation
+ *******************************************************************************/
 package io.github.pyvesb.eclipse_solargraph.utils;
 
 import org.eclipse.core.runtime.CoreException;
--- eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/utils/LaunchHelper.java
+++ eclipse-solargraph-plugin/src/main/java/io/github/pyvesb/eclipse_solargraph/utils/LaunchHelper.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2022-2024 Sean Champ and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Sean Champ - Initial implementation
+ *  Pierre-Yves B.  (pyvesdev@gmail.com) - Various improvements
+ *******************************************************************************/
 package io.github.pyvesb.eclipse_solargraph.utils;
 
 import java.io.File;
